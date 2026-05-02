ef6c55cbe8281f1581f13a0cfd35aba9e5e1e69a
Yue Fei <infyniteheap1123@outlook.com>
2026-05-02 16:05:59 +0800

    refactor: Expose main window as public static property

--- SilverWolf/App.xaml.cs
+++ SilverWolf/App.xaml.cs
@@ -3,14 +3,14 @@ using Microsoft.UI.Xaml;
 namespace SilverWolf;
 
 public partial class App : Application {
-    private Window? _window;
+    public static Window? MainWindow { get; private set; }
 
     public App() {
         InitializeComponent();
     }
 
     protected override void OnLaunched(LaunchActivatedEventArgs args) {
-        _window = new MainWindow();
-        _window.Activate();
+        MainWindow = new MainWindow();
+        MainWindow.Activate();
     }
 }
